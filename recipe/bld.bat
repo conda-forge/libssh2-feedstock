d98c972fdb813a78623801d1a1af0a5d4586637a
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-07-19 18:15:44 +0000

    Build shared libs, enable zlib compression

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,11 @@
 mkdir build && cd build
 
-cmake .. -G "NMake Makefiles"                  ^
-	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%    ^
-	-DCMAKE_BUILD_TYPE=Release                 ^
-	-DBUILD_SHARED_LIBS=OFF
+cmake .. -G "%CMAKE_GENERATOR%" ^
+	-DBUILD_SHARED_LIBS=ON ^
+	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+	-DENABLE_ZLIB_COMPRESSION=ON ^
+	-DCMAKE_BUILD_TYPE=Release
+IF %ERRORLEVEL% NEQ 0 exit 1
 
 cmake --build . --config Release --target INSTALL
+IF %ERRORLEVEL% NEQ 0 exit 1
