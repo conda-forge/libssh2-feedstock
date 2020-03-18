120b9ee73b5ac36ebe50929ea3a1be1a825276c5
Uwe L. Korn <uwe.korn@quantco.com>
2020-03-18 18:22:06 +0100

    Try Ninja on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,15 +2,14 @@ set PATH=%PREFIX%\cmake-bin\bin;%PATH%
 
 mkdir build && cd build
 
-cmake -G "%CMAKE_GENERATOR%" ^
+cmake -GNinja ^
 	  -D CMAKE_BUILD_TYPE=Release ^
 	  -D BUILD_SHARED_LIBS=ON ^
 	  -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
 	  -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
 	  -D ENABLE_ZLIB_COMPRESSION=ON ^
-	  -D CMAKE_BUILD_TYPE=Release ^
 	%SRC_DIR%
 IF %ERRORLEVEL% NEQ 0 exit 1
 
-cmake --build . --config Release --target INSTALL
+ninja install
 IF %ERRORLEVEL% NEQ 0 exit 1
